8b3d03501305702f2a150c18aac94e12a6366765
Ponnusamy K <ponnusamy.gac@gmail.com>
2016-12-20 22:02:40 +0530

    controller change for excerpt

--- app/controllers/supercache/dashboard_controller.rb
+++ app/controllers/supercache/dashboard_controller.rb
@@ -3,10 +3,21 @@ module Supercache
     layout 'supercache/application'
 
     before_filter :load_cache, only: :flip
+    before_filter :leave_out_query, only: :leave_out_flip
 
     def index
       @ar_cache = cache.read(:ar_supercache)
       @http_cache = cache.read(:http_supercache)
+    end
+
+    def leave_out_flip
+      queries = cache.read(:except)
+      if queries.try(:include?, @query)
+        queries.delete @query
+      else
+        queries.push @query
+      end
+      cache.write(:except, queries)
     end
 
     def flip
@@ -27,5 +38,10 @@ module Supercache
     def load_cache
       @cache = params[:cache]
     end
+
+    def leave_out_query
+      # @query = Digest::SHA1.hexdigest(args[0].path.to_s + args[0].body.to_s)
+    end
+
   end
 end
